353cd29920dd78eee016bf2427a82413bdfd99e2
Elvis Ngatia <72346625+blindbanditbmc@users.noreply.github.com>
2022-08-03 03:33:13 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'carhire');
+define('DB_HOST', 'sql8.freesqldatabase.com');
+define('DB_USERNAME', 'sql8510477');
+define('DB_PASSWORD', 'zVE7aGj1YU');
+define('DB_NAME', 'sql8510477');
 
 // Attempt to connect to MySQL database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
