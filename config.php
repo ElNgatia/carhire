b3aa4182fbc3601d449a44130670a952e5e19b71
Elvis Ngatia <72346625+blindbanditbmc@users.noreply.github.com>
2022-08-03 22:53:52 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_PASSWORD', 'zVE7aGj1YU');
 define('DB_NAME', 'sql8510477');
 
 // Attempt to connect to MySQL database
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Check connection
 if($link === false){
